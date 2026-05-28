cd19eedb7dc3dacb392ab1d400c50e9c59dc394e
Reponse <reponseiduha777@gmail.com>
2026-05-28 16:29:19 +0200

    feat: Add navigation tests without authentication (without logging in)

--- FrontendBackendAutomation/src/test/java/tests/navigation/NavigationTest.java
+++ FrontendBackendAutomation/src/test/java/tests/navigation/NavigationTest.java
@@ -1,42 +1,92 @@
 package tests.navigation;
 
+import base.BasePage;
 import com.microsoft.playwright.Page;
+import factories.DriverFactory;
 import org.testng.annotations.Test;
-import pages.NavigationPage;
+import pages.HomePage;
 import utils.AssertionUtils;
 import utils.HandleLoginFailure;
 
 public class NavigationTest {
 
+// Navigation with Authentication
     @Test
     public void testHomeNavigation() {
         Page page = HandleLoginFailure.setupAndNavigate();
-        NavigationPage navPage = new NavigationPage(page);
-        navPage.goToHome();
+        HomePage homePage = new HomePage(page);
+        homePage.goToHome();
         AssertionUtils.assertHomePage(page);
     }
 
     @Test
     public void testShopNavigation() {
         Page page = HandleLoginFailure.setupAndNavigate();
-        NavigationPage navPage = new NavigationPage(page);
-        navPage.goToShop();
+        HomePage homePage = new HomePage(page);
+        homePage.goToShop();
         AssertionUtils.assertShopPage(page);
     }
 
     @Test
     public void testFlashSaleNavigation() {
         Page page = HandleLoginFailure.setupAndNavigate();
-        NavigationPage navPage = new NavigationPage(page);
-        navPage.goToFlashSale();
+        HomePage homePage = new HomePage(page);
+        homePage.goToFlashSale();
         AssertionUtils.assertFlashPage(page);
     }
 
     @Test
     public void testFeaturedNavigation() {
         Page page = HandleLoginFailure.setupAndNavigate();
-        NavigationPage navPage = new NavigationPage(page);
-        navPage.goToFeatured();
+        HomePage homePage = new HomePage(page);
+        homePage.goToFeatured();
         AssertionUtils.assertFeaturedPage(page);
+    }
+
+    //Navigation without Authentication
+    @Test
+    public void testBaseToHomeNavigation() {
+        Page page = DriverFactory.initDriver();
+        BasePage basePage = new BasePage(page);
+        basePage.clickStartShopping();
+        AssertionUtils.assertHomePage(page);
+
+        DriverFactory.closeDriver();
+    }
+
+    @Test
+    public void testHomeToShopNavigation() {
+        Page page = DriverFactory.initDriver();
+        BasePage basePage = new BasePage(page);
+        HomePage homePage = new HomePage(page);
+        basePage.clickStartShopping();
+        homePage.goToShop();
+        AssertionUtils.assertShopPage(page);
+
+        DriverFactory.closeDriver();
+    }
+
+    @Test
+    public void testHomeToFlashNavigation() {
+        Page page = DriverFactory.initDriver();
+        BasePage basePage = new BasePage(page);
+        HomePage homePage = new HomePage(page);
+        basePage.clickStartShopping();
+        homePage.goToFlashSale();
+        AssertionUtils.assertFlashPage(page);
+
+        DriverFactory.closeDriver();
+    }
+
+    @Test
+    public void testHomeToFeaturedNavigation() {
+        Page page = DriverFactory.initDriver();
+        BasePage basePage = new BasePage(page);
+        HomePage homePage = new HomePage(page);
+        basePage.clickStartShopping();
+        homePage.goToFeatured();
+        AssertionUtils.assertFeaturedPage(page);
+
+        DriverFactory.closeDriver();
     }
 }

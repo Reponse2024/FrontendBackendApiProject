ccaa95ece7835fcabb9bd9ab9d8473d6552990c9
Reponse <reponseiduha777@gmail.com>
2026-06-05 10:16:38 +0200

    chore: modify frontend.yml file to run with xvfb-run mvn test

--- think-and-get-it-frontend/src/main/java/factories/DriverFactory.java
+++ think-and-get-it-frontend/src/main/java/factories/DriverFactory.java
@@ -11,7 +11,7 @@ public class DriverFactory {
 
     public static Page initDriver() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         context = browser.newContext();
         page = context.newPage();
 

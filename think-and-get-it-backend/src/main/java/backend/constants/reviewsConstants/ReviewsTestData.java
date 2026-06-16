c442d7d796744533f43a8cafaac233405fc889d1
Reponse <reponseiduha777@gmail.com>
2026-06-16 10:56:42 +0200

    chore: remove empty spaces

--- think-and-get-it-backend/src/main/java/backend/constants/reviewsConstants/ReviewsTestData.java
+++ think-and-get-it-backend/src/main/java/backend/constants/reviewsConstants/ReviewsTestData.java
@@ -4,6 +4,6 @@ public class ReviewsTestData {
     public static final int DEFAULT_RATING = 5;
     public static final String DEFAULT_TITLE = "Excellent product";
     public static final String DEFAULT_BODY = "Very satisfied with the quality!";
-    public static final String DEFAULT_PRODUCT_ID = "cb093c59-6960-4d22-950c-bd6111cb7605";
+    public static final String DEFAULT_PRODUCT_ID = "268d6b6f-f28d-4f7e-ad69-9cd4cbe03740";
 
 }
--- think-and-get-it-backend/src/test/java/wishlist/WishlistTest.java
+++ think-and-get-it-backend/src/test/java/wishlist/WishlistTest.java
@@ -4,53 +4,62 @@ import backend.implementFlow.WishlistFlow;
 import backend.constants.ResponseMessages;
 import backend.constants.HttpStatus;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import spec.SpecBuilder;
 import utils.ResponseAssertions;
+
+import java.util.List;
 
 import static spec.SpecBuilder.getRequestSpec;
 
 public class WishlistTest {
 
-    private final WishlistFlow wishlistFlow = new WishlistFlow();
-
     @Test
-    public void getWishlistSuccessfully() {
-        Response response = wishlistFlow.getWishlist(getRequestSpec());
+    public void getWishlistAfterAddingProduct() {
+        RequestSpecification spec = SpecBuilder.getRequestSpec();
+
+        new WishlistFlow().addProductToWishlist(spec);
+        Response response = new WishlistFlow().getWishlist(spec);
+
         ResponseAssertions.assertSuccess(response, HttpStatus.OK.code(), ResponseMessages.WISHLIST_FETCHED);
+        List<Object> items = response.jsonPath().getList("data");
+        Assert.assertTrue(items != null && items.size() > 0, "Wishlist should contain at least one product");
     }
+
 
     @Test
     public void addProductToWishlistSuccessfully() {
-        Response response = wishlistFlow.addProductToWishlist(getRequestSpec());
+        Response response =new WishlistFlow().addProductToWishlist(getRequestSpec());
         ResponseAssertions.assertSuccess(response, HttpStatus.CREATED.code(), ResponseMessages.WISHLIST_ADDED);
     }
 
     @Test
     public void addProductToWishlistFailsIfAlreadyExists() {
-        wishlistFlow.addProductToWishlist(getRequestSpec());
-        Response response = wishlistFlow.addProductToWishlist(getRequestSpec());
+        new WishlistFlow().addProductToWishlist(getRequestSpec());
+        Response response = new WishlistFlow().addProductToWishlist(getRequestSpec());
         ResponseAssertions.assertFailure(response, HttpStatus.CONFLICT.code(), ResponseMessages.WISHLIST_ALREADY_EXISTS);
     }
 
     @Test
     public void removeProductFromWishlistSuccessfully() {
-        wishlistFlow.addProductToWishlist(getRequestSpec());
-        Response response = wishlistFlow.removeProductFromWishlist(getRequestSpec());
+        new WishlistFlow().addProductToWishlist(getRequestSpec());
+        Response response =new WishlistFlow().removeProductFromWishlist(getRequestSpec());
         ResponseAssertions.assertSuccess(response, HttpStatus.OK.code(), ResponseMessages.WISHLIST_REMOVED);
     }
 
     @Test
     public void moveWishlistItemToCartSuccessfully() {
-        wishlistFlow.addProductToWishlist(getRequestSpec());
-        Response response = wishlistFlow.moveWishlistItemToCart(getRequestSpec());
+        new WishlistFlow().addProductToWishlist(getRequestSpec());
+        Response response = new WishlistFlow().moveWishlistItemToCart(getRequestSpec());
         ResponseAssertions.assertSuccess(response, HttpStatus.OK.code(), ResponseMessages.WISHLIST_MOVED_TO_CART);
     }
 
     @Test
     public void moveWishlistItemToCartFailsIfNotInWishlist() {
-        wishlistFlow.removeProductFromWishlist(getRequestSpec());
-        Response response = wishlistFlow.moveWishlistItemToCart(getRequestSpec());
+        new WishlistFlow().removeProductFromWishlist(getRequestSpec());
+        Response response = new WishlistFlow().moveWishlistItemToCart(getRequestSpec());
         ResponseAssertions.assertFailure(response, HttpStatus.INTERNAL_SERVER_ERROR.code(), ResponseMessages.INTERNAL_ERROR);
     }
 
